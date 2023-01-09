8acd60d0c66c81f4b7134645b5a97b331709411a
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-01-09 21:18:39 +0000

    wrote function to prompt for password options

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,6 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+var passChoiceArr = []
+var specialArr = [
   '@',
   '%',
   '+',
@@ -26,10 +27,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+var lowerCasedArr = [
   'a',
   'b',
   'c',
@@ -59,7 +60,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+var upperCasedArr = [
   'A',
   'B',
   'C',
@@ -88,47 +89,67 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+
 // Function to prompt user for password options
 function getPasswordOptions() {
 
 }
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-alert ("Hey");
-return "This is me."                                               //object, array, this method calls this method
-}
-
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
+
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
+  
+  if(correctPrompts){
+  var newPassword = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordText.value = newPassword;
+} else {
+  passwordText.value = "";
+}
+}
+
+//password based on prompts
+
+// Function to generate password with user input
+function generatePassword(){
+var password ="";
+for(var i = 0; i < charLength;i++){
+var randomIndex = Math.floor(Math.random() * passChoiceArr.length);
+password = password + passChoiceArr[randomIndex];
+}
+return password;
+}
+
+//prompt for password
+
+function getPrompts() {
+  charLength = parseInt(prompt("The number of characters in your password must be (10-64 characters)"));
+  if (isNaN(charLength) || charLength < 10 || charLength > 64) {
+    alert("Character length has to be a number, 10-64 digit, Try again");
+    return false;
+  }
+    if (confirm("Would you like lowercase letter in your password?")) {
+      passChoiceArr = passChoiceArr.concat(lowerCasedArr);
+    }
+
+    if (confirm("Would you like uppercase letter in your password?")) {
+      passChoiceArr = passChoiceArr.concat(upperCasedArr);
+    }
+    if (confirm("Would you special character in your password?")) {
+      passChoiceArr = passChoiceArr.concat(specialArr);
+    }
+
+    if (confirm("Would you like numbers in your password?")) {
+      passChoiceArr = passChoiceArr.concat(numericArr);
+    }
+    return true;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-// Code found online:
-
-
-
-function generatePassword() {
-  var length = 8,
-  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  retVal = "";
-for (var i = 0, n = charset.length; i < length; ++i) {
-  retVal += charset.charAt(Math.floor(Math.random() * n));
-}
-    return retVal;
-
-}
+generateBtn.addEventListener("click", writePassword);
